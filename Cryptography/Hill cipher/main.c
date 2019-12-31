0f1a0bf3a6d20c44aa081cf853af4e4ceb4e8586
Birat Siku <birat.sk21@gmail.com>
2019-12-31 10:34:21 +0545

    update main

--- Cryptography/Hill cipher/main.c
+++ Cryptography/Hill cipher/main.c
@@ -27,16 +27,27 @@ void pass(char a, char b){
 	
 	//printf("%d %d\n",r1,r2);
 	printf("%c%c",alpha[r1],alpha[r2]);
-	
-	
+}
+
+int inverse(int a){
+	int i,n=26,p,r;
+	for(i=1;i<n;i++){
+		p=a*i;
+		r=p%26;
+		if(r==1){
+			return i;
+		}
+	}
+	return -1;
 }
  
 
 int main(){
-	int i,j,c;
+	int i,j,c,sel,t;
 	char x='X';
 	char text[100];
 	
+	int kinv[2][2],detk,detkinv,adk[2][2];
 	//Key Input
 	
 //	printf("Key: \n");
@@ -66,11 +77,32 @@ int main(){
 	
 	printf("%s\n\n",text);
 	
-	for(i=0;i<strlen(text);i+=2){
-		if(text[i+1]!='\0')
-			pass(text[i],text[i+1]);
-		else
-			pass(text[i],x);
+	//select encryption decryption
+	printf("!-----Hill Cipher-----!");
+	printf("1.Encryption\n2.Decryption\n\nEnter choice: ");
+	scanf("%d",&sel);
+	if(sel==1){		//Encryption	
+		for(i=0;i<strlen(text);i+=2){
+			if(text[i+1]!='\0')
+				pass(text[i],text[i+1]);
+			else
+				pass(text[i],x);
+		}
+	}
+	else if(sel==2){			//Decryption
+		detk=key[0][0]*key[1][1]-key[0][1]*key[1][0];
+		if(detk > 0){
+			detkinv=inverse(detk);
+		}
+		else{
+			detk*=-1;
+			t=((detk%26)-26)*-1;
+			detkinv=inverse(t);
+		}
+		
+	}
+	else{
+		printf("Make a valid choice!!");
 	}
 	
 	return 0;
